e1516282fddd582d85d83cdaafecdb0e9b234447
litingrui <1484761461@qq.com>
2017-03-21 11:05:37 +0800

    Add files via upload

---
+++ exercise_list.py
@@ -0,0 +1,12 @@
+#-- coding:utf-8 --
+L = [
+['Apple','Google','Microsoft'],
+['Java','Python','Ruby','Php'],
+['Adam','Bart','Lisa']
+]
+#print Apple
+print(L[0][0])
+#print Python
+print(L[1][1])
+#print Lisa
+print(L[2][2])
